0f8d1b38121cefa3990372798a5b053ec11d65b7
Justin Rogers <justin.a.rogers@gmail.com>
2013-10-06 20:46:22 -0500

    Gummy Fied

--- tg.mk
+++ tg.mk
@@ -1,16 +1,16 @@
 $(call inherit-product, device/samsung/jfltecri/full_jfltecri.mk)
 
-# Inherit some common CM stuff.
-$(call inherit-product, vendor/cm/config/cdma.mk)
+# Inherit some common TG stuff.
+$(call inherit-product, vendor/Gummy/config/cdma.mk)
 
 # Enhanced NFC
-$(call inherit-product, vendor/cm/config/nfc_enhanced.mk)
+$(call inherit-product, vendor/Gummy/config/nfc_enhanced.mk)
 
-# Inherit some common CM stuff.
-$(call inherit-product, vendor/cm/config/common_full_phone.mk)
+# Inherit some common TG stuff.
+$(call inherit-product, vendor/Gummy/config/common_full_phone.mk)
 
 PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=jfltecri TARGET_DEVICE=jfltecri BUILD_FINGERPRINT="samsung/jfltecri/jfltecri:4.2.2/JDQ39/R970CVVUAME4:user/release-keys" PRIVATE_BUILD_DESC="jfltecri-user 4.2.2 JDQ39 R970CVVUAME4 release-keys"
 
-PRODUCT_NAME := cm_jfltecri
+PRODUCT_NAME := tg_jfltecri
 PRODUCT_DEVICE := jfltecri
 
